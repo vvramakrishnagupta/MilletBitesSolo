a19ae6b14e1a514e760869ce0fa2855c869856c2
vvramakrishnagupta <vvramakrishnagupta@gmail.com>
2025-08-03 15:06:18 -0600

    Update cart.js

--- cart.js
+++ cart.js
@@ -9,11 +9,6 @@ export function addToCart(product, quantity) {
   }
 }
 
-export function removeFromCart(productName) {
-  const idx = cart.findIndex(item => item.name === productName);
-  if (idx > -1) cart.splice(idx, 1);
-}
-
 export function getCart() {
   return cart;
 }
